f517b615eccc0b6199ee272200553b12acbe7666
muradhossin <murad15-2547@diu.edu.bd>
2024-01-06 16:49:47 +0600

    user model

---
+++ src/main/java/com/example/busreservation/entities/User.java
@@ -0,0 +1,17 @@
+package com.example.busreservation.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private  Integer id;
+    private  String userName;
+    private  String password;
+    private  String role;
+}
