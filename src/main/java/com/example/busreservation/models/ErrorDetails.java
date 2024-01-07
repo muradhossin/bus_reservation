d6e8935751209829b68da94dcb8f3e237ac36627
muradhossin <murad15-2547@diu.edu.bd>
2024-01-07 14:08:58 +0600

    exception handling

---
+++ src/main/java/com/example/busreservation/models/ErrorDetails.java
@@ -0,0 +1,17 @@
+package com.example.busreservation.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+    private int errorCode;
+    private  String errorMessage;
+    private  String devErrorMessage;
+    private  Long timestamp;
+}
