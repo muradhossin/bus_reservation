195d0e98dedd7c2ef9ac1483006d6a76f520e442
muradhossin <murad15-2547@diu.edu.bd>
2024-01-12 21:30:06 +0600

    bus schedule controller

---
+++ src/main/java/com/example/busreservation/controller/BusScheduleController.java
@@ -0,0 +1,35 @@
+package com.example.busreservation.controller;
+
+import com.example.busreservation.entities.BusSchedule;
+import com.example.busreservation.models.ResponseModel;
+import com.example.busreservation.services.BusScheduleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/schedule")
+public class BusScheduleController {
+    @Autowired
+    private BusScheduleService busScheduleService;
+
+    @PostMapping("/add")
+    public ResponseModel<BusSchedule> addBusSchedule(@RequestBody  BusSchedule busSchedule) {
+        final  BusSchedule schedule = busScheduleService.addSchedule(busSchedule);
+        return new ResponseModel<>(HttpStatus.OK.value(), "Schedule Saved", schedule);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<BusSchedule>> getAllSchedules() {
+        return  ResponseEntity.ok(busScheduleService.getAllBusSchedules());
+    }
+
+    @GetMapping("/{routeName}")
+    public  ResponseEntity<List<BusSchedule>> getBusScheduleByRouteName(@PathVariable(name = "routeName") String routeName) {
+        return ResponseEntity.ok(busScheduleService.getSchedulesByRoute(routeName));
+    }
+
+}
