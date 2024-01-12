a88dd36bffa42fc7cdcb5ad2309a92ae747ca0e7
muradhossin <murad15-2547@diu.edu.bd>
2024-01-12 20:33:59 +0600

    bus schedule serviceImpl and error fix

--- src/main/java/com/example/busreservation/repos/BusScheduleRepository.java
+++ src/main/java/com/example/busreservation/repos/BusScheduleRepository.java
@@ -5,9 +5,10 @@ import com.example.busreservation.entities.BusRoute;
 import com.example.busreservation.entities.BusSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface BusScheduleRepository extends JpaRepository<BusScheduleRepository, Long> {
-    Optional<BusSchedule> findByBusRoute(BusRoute busRoute);
+public interface BusScheduleRepository extends JpaRepository<BusSchedule, Long> {
+    Optional<List<BusSchedule>> findByBusRoute(BusRoute busRoute);
     Boolean existsByBusAndBusRouteAndDepartureTime(Bus bus, BusRoute busRoute, String date);
 }
