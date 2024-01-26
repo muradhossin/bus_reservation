b59fbf4d9f8aecb415ff25623a5a08a9296bf866
muradhossin <murad15-2547@diu.edu.bd>
2024-01-26 20:39:09 +0600

    customer entity update and CustomerRepo added

--- src/main/java/com/example/busreservation/entities/Customer.java
+++ src/main/java/com/example/busreservation/entities/Customer.java
@@ -1,5 +1,6 @@
 package com.example.busreservation.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,15 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "customer")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long customerId;
     private  String customerName;
+    @Column(unique = true)
     private  String mobile;
+    @Column(unique = true)
     private  String email;
 }
---
+++ src/main/java/com/example/busreservation/repos/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.example.busreservation.repos;
+
+import com.example.busreservation.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByMobileOrEmail(String mobile, String email);
+    Optional<Customer> findByMobile(String mobile);
+    Optional<Customer> findByEmail(String email);
+    Boolean existsByMobile(String mobile);
+    Boolean existsByEmail(String Email);
+    Boolean existsByMobileOrEmail(String mobile, String Email);
+}
