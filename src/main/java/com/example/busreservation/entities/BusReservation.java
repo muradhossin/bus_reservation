2e2bc3fc102f025e24deafb1abf3cb21c91bcd62
muradhossin <murad15-2547@diu.edu.bd>
2024-01-26 20:49:40 +0600

    busReservation entity modify

--- src/main/java/com/example/busreservation/entities/BusReservation.java
+++ src/main/java/com/example/busreservation/entities/BusReservation.java
@@ -1,5 +1,6 @@
 package com.example.busreservation.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,11 +10,19 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "bus_reservation")
 public class BusReservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long busReservationId;
+    @OneToOne
+    @JoinColumn(name = "customer_id")
     public  Customer customer;
+    @OneToOne
+    @JoinColumn(name = "bus_schedule_id")
     private  BusSchedule busSchedule;
-    private  Integer timestamp;
+    private  Long timestamp;
     private  String departureDate;
     private  Integer totalSeatBooked;
     private  String seatNumbers;
